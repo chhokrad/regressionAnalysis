7677a8b43b8416026ef1163816791363d7d34952
chhokrad <ajay.d.chhokra@vanderbilt.edu>
2016-12-12 11:52:08 -0600

    Add files via upload

---
+++ my_namedtuples.py
@@ -0,0 +1,3 @@
+from collections import namedtuple
+XYdata = namedtuple('XYdata',['X_prev_frame','X_new_frame','Y_frame'])
+FeatureSelection = namedtuple('FeatureSelection',['Sorted_feature_vector', 'Score'])
